243f71f82f1d18c66a68b8044496d93178999c32
Yemuri note <yemuri.teodoro@gmail.com>
2024-04-21 18:41:39 -0300

    branch leite e inicioleite criados

---
+++ frontend/src/pages/leite/inicio/inicioLeite.js
@@ -0,0 +1,12 @@
+
+
+
+export default function InicioLeite(){
+
+    return(
+        <div>
+            <h1>Bem vindo a Casa do Leite</h1>
+           
+        </div>
+    )
+}
--- frontend/src/pages/routes.js
+++ frontend/src/pages/routes.js
@@ -1,6 +1,8 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Inicial from './home/inicial'
 import Login from './login/paginaLogin'
+import InicioLeite from './leite/inicio/inicioLeite'
+
 
 
 export default function Rotas (){
@@ -10,6 +12,9 @@ export default function Rotas (){
                 <Routes>
                     <Route path='/' element={<Inicial />}/>
                     <Route path='/login' element={<Login />}/>
+                    <Route path='/leite' element={<InicioLeite />}/>
+                    
+                    
                 </Routes>
             </BrowserRouter>
         </div>
