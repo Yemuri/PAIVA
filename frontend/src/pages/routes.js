00fbeea91a99d22b1b422bc392e26accd3958ebd
CamilaRaimundo <camila.raimundo@unesp.br>
2024-05-19 18:38:47 -0300

    19.05.2024
    Criação da página de prestação de contas.

--- frontend/src/pages/routes.js
+++ frontend/src/pages/routes.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Inicial from './home/inicial'
 import Login from './login/paginaLogin'
 import InicioLeite from './leite/inicio/inicioLeite'
-
+import InicioContas from './contas/inicioContas'
 
 
 export default function Rotas (){
@@ -13,6 +13,7 @@ export default function Rotas (){
                     <Route path='/' element={<Inicial />}/>
                     <Route path='/login' element={<Login />}/>
                     <Route path='/leite' element={<InicioLeite />}/>
+                    <Route path='/prestacao-de-contas' element={<InicioContas />}/>
                     
                     
                 </Routes>
