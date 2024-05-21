2142a36fdd801baaff4e98509603df22a75847b0
Yemuri note <yemuri.teodoro@gmail.com>
2024-05-20 21:34:33 -0300

    alterado footer

--- frontend/src/component/footer/footer.js
+++ frontend/src/component/footer/footer.js
@@ -26,76 +26,56 @@ export default function Footer() {
         setModalAberto(false)
     }
 
-
-
-
-
-
-
     return (
-        <div className="container-geral">
-            {/* <h3>Desenvolvido por: jorginho_dev_o_REI_dos_codigos</h3> */}
-            {/* <div className='topo'>
-                <FaArrowAltCircleUp />
-            </div> */}
-            <div className='colunas'>
-                {/* <div className='row-footer'>
-                    <h3>Desenvolvedores</h3>
-                    <ul>
-                        <li>Bruna Raissa Correa Moura</li>
-                        <li>Camila Pereira Raimundo</li>
-                        <li>Eduardo Coque Corrêa de Lima</li>
-                        <li>Fernando Eduardo Motta Mendes</li>
-                        <li>Karoliny Bazan Silva</li>
-                        <li>Kauã Limão Nunes</li>
-                        <li>Thais Carolina Henrique</li>
-                        <li>Yemuri Wanderlei Teodoro</li>
-                    </ul>
-                </div> */}
-                <div className='row-footer'>
-                    <h3>Navegação</h3>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Casa do Leite</Link></li>
-                        <li><Link to="/">Casa da Criança</Link></li>
-                        <li><Link to="/">Abrigo de Idosos</Link></li>
-                    </ul>
-                </div>
-                {/* <div className='row-footer'>
-                    <FaRegArrowAltCircleUp />
-                </div> */}
-                <div className='contact'>
-                    <h3>Contato</h3>
-                    <div className='container_icones'>
-                    <span className='icon_contact'><Link to=''><FaInstagramSquare /></Link></span>
-                    <span className='icon_contact'><Link to=''><FaFacebook /></Link></span>
-                    <span className='icon_contact'><Link to=''><MdEmail /></Link></span>
-                    <span className='icon_contact'><Link to=''><FaGoogle /></Link></span>
-                    <span className='icon_contact'><Link to=''><BsFillTelephoneFill /></Link></span>
+        <div>
+            <div className="container-geral">
+                <div className='colunas'>
+                    <div className='row-footer'>
+                        <h3>Navegação</h3>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">Casa do Leite</Link></li>
+                            <li><Link to="/">Casa da Criança</Link></li>
+                            <li><Link to="/">Abrigo de Idosos</Link></li>
+                        </ul>
                     </div>
-                </div>
-            </div>
-            <div className='copyright'>
-                <div><h6>Copyright © 2024 - Todos os direitos reservados</h6></div>
-                <div className='devs'>
-                    <a className='botaoabrirmodal' onClick={abrirModal}>Devs</a>
-                    <Modal isOpen={modalAberto}>
-                        {/* <h6>MODAL ABERTO</h6> */}
-                        <div>
-                            <h2>Desenvolvedores</h2>
-                            <a href='mailto: camispeereira@gmail.com'> <p>Bruna</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Camila</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Fernando</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Karol</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Kaua</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Thais</p></a>
-                           <a href='mailto: email@gmail.com'> <p>Yemuri</p></a>
+                    <div className='contact'>
+                        <h3>Contato</h3>
+                        <div className='container_icones'>
+                            <span className='icon_contact'><Link to=''><FaInstagramSquare /></Link></span>
+                            <span className='icon_contact'><Link to=''><FaFacebook /></Link></span>
+                            <span className='icon_contact'><Link to=''><MdEmail /></Link></span>
+                            <span className='icon_contact'><Link to=''><FaGoogle /></Link></span>
+                            <span className='icon_contact'><Link to=''><BsFillTelephoneFill /></Link></span>
                         </div>
-                        <button className='fecharmodals' onClick={fecharModal}>{'❌'}</button>
-                    </Modal>
+                            <h6 >Copyright</h6>
+                    </div>
+                    <div className='devpor'>
+                        <h6>Desenvolvido por <a onClick={abrirModal} className='fabrica-software'> Fábrica de Software</a></h6>
+                    </div>
+                        <Modal isOpen={modalAberto} onRequestClose={fecharModal} className='container-desenvolvedores'>
+                            <div className='container-texto'>
+                            <h3>Desenvolvedores:</h3>
+
+                                <a href='mailto: camispeereira@gmail.com'> <p>Bruna</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Camila</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Eduardo</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Fernando</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Karol</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Kaua</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Thais</p></a>
+                                <a href='mailto: email@gmail.com'> <p>Yemuri</p></a>
+                            </div>
+                            
+                        </Modal>
                 </div>
+
             </div>
-        </div> // container geral
+
+
+
+
+        </div > // container geral
 
     )
 }
