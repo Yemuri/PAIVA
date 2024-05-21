6ec5cd7cd0aa364814b49677f1101b6cff214b7a
Thais <thais.carolina.henrique@gmail.com>
2024-05-21 16:32:06 -0300

    inicio casa do leite

--- frontend/src/pages/leite/inicio/inicioLeite.js
+++ frontend/src/pages/leite/inicio/inicioLeite.js
@@ -1,10 +1,20 @@
 
-import "./leite.css";
+import "./leite.css"
 
 export default function InicioLeite() {
   return (
     <div>
       <h1>Bem vindo a Casa do Leite</h1>
+      <section>
+
+      
+      <div>
+
+
+      <p>Fundação Espírita “Sebastião Paiva” é uma entidade filantrópica sem fins lucrativos que faz parte da rede de Proteção Social da Política Nacional de Assistência Social. Iniciou suas atividades em julho de 2007 com o Programa de Atendimento à Família, realizando visitas domiciliares e estudos socioeconômicos nas famílias atendidas através da doação de gêneros alimentícios. Em agosto de 2007, a fundação começou a formar grupos temáticos, inicialmente com duas turmas. Cada grupo tem duração de três meses, podendo ser prorrogado por mais três meses, se necessário.  Atualmente, a fundação atende 50 famílias e 70 crianças com doações de alimentos, visando inseri-las no mercado de trabalho. Para isso, desenvolveu um projeto de Geração de Renda e Oportunidade de Trabalho, localizado na Casa do Leite, na Rua Arnaldo Rodrigues de Menezes, 15-91, Pq. Jaraguá. O projeto oferece Cursos Livres, Oficinas de Geração de Renda e orientações para gestantes.
+        </p>
+      </div>
+      </section>
     </div>
   );
 }
