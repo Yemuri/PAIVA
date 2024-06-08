f1dece9351516ec76b9b33820162e29bc76f0a62
Kauã Limão <kauaelucas81@gmail.com>
2024-06-08 14:01:01 -0300

    pequenas alterações home

--- frontend/src/pages/contas/inicioContas.js
+++ frontend/src/pages/contas/inicioContas.js
@@ -5,11 +5,6 @@ import { FaFilePdf } from "react-icons/fa6";
 export default function InicioContas(){
     return(
         <div>
-
-            <div className="nav">
-                <h1>FINGE QUE ISSO É UM CABEÇALHO</h1>
-            </div>
-{/* começar a partir daqui - essa classe "container-container" deve ser a principal*/}
             <div className="container-contas">
                 <div className="title_contas">
                     <h2>Prestação de Contas</h2>
